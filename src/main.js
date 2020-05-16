cb004a94190a32c941f580a78259f08f708fbab7
Nathan Altice <naltice@ucsc.edu>
2020-05-16 15:54:50 -0700

    initial commit - file structure

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// Nathan Altice
+// Created: 5/16/20
+// Updated: 5/16/20
+// Particle Emitters & Effects
+// 
+
+// de-bong the JavaShrek ogre
+'use strict';
+
+// define game object
+let config = {
+    type: Phaser.AUTO,
+    width: 200,
+    height: 200
+}
+
+const game = new Phaser.Game(config);
+
+// some globals
