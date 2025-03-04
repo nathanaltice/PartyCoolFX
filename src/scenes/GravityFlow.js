e2fcded44f1431306f2bfb45d2aa809e450d3a68
Nathan Altice <60799150+nathanaltice@users.noreply.github.com>
2025-03-04 10:57:38 -0800

    phaser version update plus death zone example fix

--- src/scenes/GravityFlow.js
+++ src/scenes/GravityFlow.js
@@ -10,10 +10,11 @@ class GravityFlow extends Phaser.Scene {
     }
 
     create() {
-        // create a 💀 PARTICLE DEATH ZONE 💀
-        this.deathZone = new Phaser.Geom.Circle(0, 0, 100)
+        // create a 💀 PARTICLE DEATH ZONES 💀
+        this.movingDeathZone = new Phaser.Geom.Circle(0, 0, 100)
+        this.staticdeathZone = new Phaser.Geom.Rectangle(100, 50, 100, 100)
         
-        // init graphics to draw DEATH ZONE (in update)
+        // init graphics to draw DEATH ZONES (in update)
         this.gfx = this.add.graphics()
 
         // set up particle emitter
@@ -21,12 +22,12 @@ class GravityFlow extends Phaser.Scene {
             angle: { min: 180, max: 360, }, // try adding steps: 1000 👍
             speed: { min: 10, max: 500, steps: 5000 },
             gravityY: -350,
-            gravityX: 1000,
+            gravityX: -300,
             lifespan: 4000,
             quantity: 6,
             scale: { start: 0.1, end: 2 },
             tint: [ 0xffff00, 0xff0000, 0x00ff00, 0x00ffff, 0x0000ff ],
-            deathZone: { type: 'onEnter', source: this.deathZone }
+            deathZone: [ this.staticdeathZone, this.movingDeathZone ]
         })
 
         this.deathEmitter = this.add.particles(0, 0, 'cross', {
@@ -36,20 +37,21 @@ class GravityFlow extends Phaser.Scene {
             quantity: 250,
             lifespan: 750,
             frequency: -1,
-            emitZone: { type: 'edge', source: this.deathZone, quantity: 250 },
+            emitZone: { type: 'edge', source: this.movingDeathZone, quantity: 250 },
             tint: 0xFFFF00
         })
 
         // add mouse move listener for movable death zone
         this.input.on('pointermove', (pointer) => {
-            this.deathZone.x = pointer.x
-            this.deathZone.y = pointer.y
+            this.movingDeathZone.x = pointer.x
+            this.movingDeathZone.y = pointer.y
         })
+
         // add mouse input listener to invert death zone type and fire particles
         this.input.on('pointerdown', (pointer) => {
-            // toggle death zone
-            this.gravityEmitter.deathZones[0].killOnEnter ? this.gravityEmitter.deathZones[0].killOnEnter = false : this.gravityEmitter.deathZones[0].killOnEnter = true 
-            // fire death particles
+            // toggle moving death zone
+            this.gravityEmitter.deathZones[1].killOnEnter ? this.gravityEmitter.deathZones[1].killOnEnter = false : this.gravityEmitter.deathZones[1].killOnEnter = true 
+            // fire death particles from moving death zone
             this.deathEmitter.emitParticleAt(pointer.x, pointer.y)
         })
 
@@ -64,8 +66,10 @@ class GravityFlow extends Phaser.Scene {
     update() {
         // draw DEATH ZONE
         this.gfx.clear()
-        this.gfx.lineStyle(1, 0xFFFF00, 1)
-        this.gfx.strokeCircleShape(this.deathZone)
+        this.gfx.lineStyle(1, 0xFFFF00, 2)
+        this.gfx.strokeCircleShape(this.movingDeathZone)
+        this.gfx.lineStyle(1, 0x00FF00, 2)
+        this.gfx.strokeRectShape(this.staticdeathZone)
 
         // scene switching / restart
         if(Phaser.Input.Keyboard.JustDown(this.reload)) {
